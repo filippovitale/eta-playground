4ae569468ca2d07c12c165b09aa7cb30cc2d7955
Filippo Vitale <fvitale@gmail.com>
2017-03-30 09:19:44 -0400

    use the right character for Tømme

--- ghc-derive/Main.hs
+++ ghc-derive/Main.hs
@@ -1,20 +1,20 @@
 {-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
 module Main where
 
-data Liste a = Tomme | Kons a (Liste a)
+data Liste a = Tømme | Kons a (Liste a)
               deriving (Eq, Show,
                         Functor,
                         Foldable,
                         Traversable)
 
 -- instance Foldable Liste where
---   foldr _ z Tomme = z
+--   foldr _ z Tømme = z
 --   foldr f z (Kons x xs) = f x $ foldr f z xs
 --
 -- instance Traversable Liste where
---   traverse _ Tomme = pure Tomme
+--   traverse _ Tømme = pure Tømme
 --   traverse f (Kons x xs) = Kons <$> f x <*> traverse f xs
 
 main :: IO ()
 main = print $ concat l
-  where l = Kons "a" $ Kons "b" $ Kons "c" Tomme
+  where l = Kons "a" $ Kons "b" $ Kons "c" Tømme
